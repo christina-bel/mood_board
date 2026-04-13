33494bf64baf21e3d182913c0f17acd70e0db25b
kristina <belova.kristina20@wb.ru>
2026-04-13 13:52:59 +0300

    feat(theme): implement app theme and colors

---
+++ lib/ui/core/themes/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData dark() => ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: const ColorScheme.dark(
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
+        ),
+        cardColor: AppColors.card,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: AppColors.textPrimary),
+          bodySmall: TextStyle(color: AppColors.textSecondary),
+        ),
+      );
+}
